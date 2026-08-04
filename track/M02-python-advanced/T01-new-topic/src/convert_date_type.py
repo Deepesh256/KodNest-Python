509e13235fccaa7c5fcb73c14041d454713b7574
Deepesh <deepeshkumar6300@gmail.com>
2026-08-04 17:34:24 +0530

    Add add_two_numbers.py, age_find.py, convert_date_type.py, data_type_conversion.py, data_types.py, division_floor_division.py

---
+++ track/M02-python-advanced/T01-new-topic/src/convert_date_type.py
@@ -0,0 +1,16 @@
+# Read and Convert Student Details
+
+# Read inputs
+student_name = input()
+student_age = int(input())
+course_rating = float(input())
+
+# Display values
+print(f"Student: {student_name}")
+print(f"Age: {student_age}")
+print(f"Rating: {course_rating}")
+
+# Display data types
+print(type(student_name))
+print(type(student_age))
+print(type(course_rating))
---
+++ track/M02-python-advanced/T01-new-topic/src/data_type_conversion.py
@@ -0,0 +1,17 @@
+age_text = "21"
+course_fee_text = "499.50"
+attempts = 3
+
+# Convert the values here
+age = int(age_text)
+course_free =float(course_fee_text)
+attempts_text = str(attempts)
+
+
+# Display the converted values and their data types
+print(age)
+print(type(age))
+print(course_free)
+print(type(course_free))
+print(attempts_text)
+print(type(attempts_text))
