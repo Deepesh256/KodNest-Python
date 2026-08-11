177812439b9c4d2c792c04a49a1d5afd17739458
Deepesh <deepeshkumar6300@gmail.com>
2026-08-12 01:46:46 +0530

    Add program to update tuple

---
+++ track/M02-python-advanced/T01-new-topic/src/update_tuple.py
@@ -0,0 +1,19 @@
+# Read the course details
+course_name = input()
+current_week =input()
+course_status =input()
+
+# Create the original tuple
+course_details=(course_name,current_week,course_status)
+
+
+# Read the updated week
+updated_week = input()
+
+
+# Create and assign a new tuple
+course_details = (course_name, updated_week,course_status)
+
+
+# Display the updated tuple
+print(course_details)
