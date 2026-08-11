377d0361f359db9c6ade474fd83d990ddb4ea02a
Deepesh <deepeshkumar6300@gmail.com>
2026-08-12 01:49:38 +0530

    Update simple calculator function logic

---
+++ track/M02-python-advanced/T01-new-topic/src/simple_calculator_function.py
@@ -0,0 +1,18 @@
+def calculate(first_number, second_number, operator):
+    if operator == '+':
+        return first_number + second_number
+    elif operator == '-':
+        return first_number - second_number
+    elif operator == '*':
+        return first_number * second_number
+    elif operator == '/':
+        return first_number / second_number
+    else:
+        return "Invalid operator"
+
+first_number = int(input())
+second_number = int(input())
+operator = input().strip()
+
+result = calculate(first_number, second_number, operator)
+print(result)
