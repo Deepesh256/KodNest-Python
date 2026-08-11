db1e2252395e2e00b10cbea728556228d63a8c40
Deepesh <deepeshkumar6300@gmail.com>
2026-08-12 01:45:28 +0530

    Add program to check even number

---
+++ track/M02-python-advanced/T01-new-topic/src/even_number.py
@@ -0,0 +1,14 @@
+# Read the limit
+
+limit = int(input())
+# Initialize the loop variable and total
+number = 1
+total = 0
+
+# Examine every number from 1 to limit
+while number <= limit:
+    if number % 2 == 0:
+        total += number
+    number +=1        
+# Display the result
+print("Even Sum:",total)
